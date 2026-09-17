b5f94f9ca8d8a6ed601b6a03b3e2261637c5fde0
Luis Oliveira <mvp.oliveira55@gmail.com>
2026-09-17 23:40:55 +0100

    adiciona funcionalidade ao menu, ativam o hover do menu quando estiver acessando respectivas paginas

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+        target: '#nav-bar',
+        offset: 100
+    });
+});
